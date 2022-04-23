c0918e456a750596e5aaf51f65ad95302f99260e
lukasz.klejszta <lukasz.klejszta@tsh.io>
2022-04-23 17:50:37 +0200

    feat: add dev dependencies and config

---
+++ README.md
@@ -0,0 +1,3 @@
+### Proxy Handler Generic Execution
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,80 @@
 export type CallbackDataType = string;
 export type CallbackDataKey = string;
+export type FunctionCallbackDataArg = unknown;
+export type OnSuccessDataResult = any;
+export type PromiseEnding = string;
+export enum Tag {
+  sync = "SYNC",
+  async = "ASYNC",
+}
 export interface CallbackData {
   type: CallbackDataType;
   key: CallbackDataKey;
 }
-export enum Tag {
-    sync = 'SYNC',
-    async = 'ASYNC',
-}
-export type FunctionCallbackDataArg = unknown;
 export interface FunctionCallbackData {
   tag: Tag;
   args: FunctionCallbackDataArg[];
 }
-export type onErrorData = CallbackData & FunctionCallbackData & { error: Error };
-export type onSuccessDataResult = any;
-export type onSuccessData = CallbackData & FunctionCallbackData & { result: onSuccessDataResult };
-export type onNonFunctionData = CallbackData;
-export type onError = (data: onErrorData) => Error | void;
-export type onSuccess = (data: onSuccessData) => void;
-export type onNonFunction = (data: onNonFunctionData) => void;
-export type promiseEnding = string;
+export interface OnErrorAdditionalData {
+  error: Error;
+}
+export interface OnSuccessAdditionalData {
+  result: OnSuccessDataResult;
+}
+export type OnErrorData = CallbackData & FunctionCallbackData & OnErrorAdditionalData;
+export type OnSuccessData = CallbackData & FunctionCallbackData & OnSuccessAdditionalData;
+export type OnNonFunctionData = CallbackData;
+export type OnError = (data: OnErrorData) => Error | void;
+export type OnSuccess = (data: OnSuccessData) => void;
+export type OnNonFunction = (data: OnNonFunctionData) => void;
 export interface ProxyHandlerGenericExecutionOptions {
-  onError: onError;
-  onSuccess: onSuccess;
-  onNonFunction: onNonFunction;
-  promiseEnding?: promiseEnding;
-
- }
+  onError: OnError;
+  onSuccess: OnSuccess;
+  onNonFunction: OnNonFunction;
+  /**
+   * @description
+   *`promiseEnding` field is used when an object (on which this proxy handler is applied) function use callback style to return value. It should be a name of a function in result which transforms async processing from callback style to promise style.
+   * @example
+   * ```
+   * import { StepFunctions } from "aws-sdk";
+   * import { proxyHandlerGenericExecution } from "proxy-handler-generic-execution";
+   *
+   * const promiseEnding = "promise";
+   * const stepFunctions = new StepFunctions();
+   * const wrappedStepFunctions = new Proxy(
+   *   stepFunctions,
+   *   proxyHandlerGenericExecution({
+   *     promiseEnding,
+   *     onSuccess: () => {},
+   *     onNonFunction: () => {},
+   *     onError: () => {},
+   *   }),
+   * );
+   *
+   * (async () => {
+   *   await wrappedStepFunctions.startExecution({ stateMachineArn: "ARN" })[promiseEnding]();
+   * })();
+   * ```
+   */
+  promiseEnding?: PromiseEnding;
+}
 export type ProxyHandlerGenericExecution = (options: ProxyHandlerGenericExecutionOptions) => ProxyHandler<any>;
 
- export const executionGenericProxyHandler: ProxyHandlerGenericExecution = (options) => ({
+export const proxyHandlerGenericExecution: ProxyHandlerGenericExecution = (options) => ({
   get: (target, key) => {
     const type = typeof target[key];
     if (type !== "function") {
       options.onNonFunction({ key: String(key), type });
       return target[key];
     }
-    return (...args: FunctionCallbackData['args']) => {
+    return (...args: FunctionCallbackData["args"]) => {
       try {
         const syncResult = target[key](...args);
         const promiseEnding = options.promiseEnding;
         if (promiseEnding !== undefined && typeof syncResult[promiseEnding] === "function" && type === "function") {
           return {
             ...syncResult,
-            [promiseEnding]: async (...args2: FunctionCallbackData['args']) => {
+            [promiseEnding]: async (...args2: FunctionCallbackData["args"]) => {
               try {
                 const asyncResult = await syncResult[promiseEnding](...args2);
                 options.onSuccess({ args, result: asyncResult, type, key: String(key), tag: Tag.async });
